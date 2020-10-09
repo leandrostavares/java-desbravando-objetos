9d4307cf55bb0ae5790d0caca30527bfb4dec963
Tavares <leandro.tavares@sap.com>
2020-10-08 21:59:56 -0300

    Operações usando polimorfismo
    Alteramos a classe CarrinhoDeCompras para que aplique um desconto de 5% em cada livro adicionado, além de armazenar o valor total da compra
    
    Na classe registro de vendas adicionamos 2 tipos diferentes de livro: LivroFisico e Ebook. Neste cenário somente podemos usar métodos que existam na class Livro, pois a nossa referência é para este tipo.
    
    Apesar de estarmos usando polimorfismo ao tratar todos como 'Livro', o método 'AplicaDescontoDe' será o do tipo específico, pois a a JVM realiza essa escolhe em tempo de execução e não de compilação.

--- src/RegistroDeVendas.java
+++ src/RegistroDeVendas.java
@@ -6,13 +6,17 @@ public class RegistroDeVendas {
 
         LivroFisico fisico = new LivroFisico(autor);
         fisico.setNome("Test-Driven Development");
+        fisico.setValor(59.90);
 
         Ebook ebook = new Ebook(autor);
         ebook.setNome("Test-Driven Development");
+        ebook.setValor(29.90);
 
         CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
         carrinhoDeCompras.adiciona(fisico);
         carrinhoDeCompras.adiciona(ebook);
+
+        System.out.println(carrinhoDeCompras.getValorTotalDaCompra());
 
     }
 }
