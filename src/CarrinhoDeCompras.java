835722b7231e0f943bd1128136ec36552e2de61f
Tavares <leandro.tavares@sap.com>
2020-10-08 21:32:36 -0300

    Adicionado mais um tipo de livro: LivroFisico. Devido a somente ele possir uma taxa de impressão, faz sentido que seja um novo tipo.
    Adicioado um tipo para servir de carrinho de compras onde criamos uma sobrecarga do metodo adiciona para cada tipo que desejamos
    
    Inicialmente parece uma boa ideia mas para cada tipo novo teremos que criar um método novo

---
+++ src/CarrinhoDeCompras.java
@@ -0,0 +1,11 @@
+public class CarrinhoDeCompras {
+
+    public void adiciona(LivroFisico livro) {
+        System.out.println("Adicionando livro: " + livro);
+    }
+
+    public void adiciona(Ebook livro) {
+        System.out.println("Adicionando livro: " + livro);
+    }
+
+}
---
+++ src/RegrasDeDesconto.java
@@ -0,0 +1,26 @@
+public class RegrasDeDesconto {
+
+    public static void main(String[] args) {
+
+        Autor autor = new Autor();
+        autor.setNome("Rodrifo Turini");
+
+        Livro livro = new Livro(autor);
+        livro.setValor(59.90);
+
+        if (livro.aplicaDescontoDe(0.3)) {
+            System.out.println("Valor do livro com desconto " + livro.getValor());
+        } else {
+            System.out.println("Desconto do livro não pode ser maior que 30%");
+        }
+
+        Ebook ebook = new Ebook(autor);
+        ebook.setValor(100);
+
+        if (ebook.aplicaDescontoDe(0.20)) {
+            System.out.println("Valor do ebook com desconto " + ebook.getValor());
+        } else {
+            System.out.println("Desconto do ebook não pode ser maior que 15%");
+        }
+    }
+}
