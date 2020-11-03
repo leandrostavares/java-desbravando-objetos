6fc37f3cc8363f6f79e064bb2dd2b488e0e0f577
Tavares <leandro.tavares@sap.com>
2020-11-03 08:34:48 -0300

    Criado uma exception Criamos um sub-tipo específico de RuntimeException (unchecked exception) para evitar que um livro seja criado com uma referência NULL de autor

---
+++ src/br/com/casadocodigo/exceptions/AutorNuloException.java
@@ -0,0 +1,7 @@
+package br.com.casadocodigo.exceptions;
+
+public class AutorNuloException extends RuntimeException {
+    public AutorNuloException(String message) {
+        super(message);
+    }
+}
