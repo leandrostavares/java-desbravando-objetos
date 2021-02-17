e42dc7de4b766b48ad1ce9c4c5d37c5cd5a257df
Tavares <leandro.tavares@sap.com>
2021-02-17 16:04:52 -0300

    Ordenação com java 7 para para adicionar critérios através de novas instâncias que implementem a interface Comparator

---
+++ src/br/com/casadocodigo/teste/NovidadesJava8.java
@@ -0,0 +1,44 @@
+package br.com.casadocodigo.teste;
+
+import br.com.casadocodigo.livraria.Autor;
+import br.com.casadocodigo.produtos.Livro;
+import br.com.casadocodigo.produtos.LivroFisico;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+class ComparadorPorNome implements Comparator<Livro>{
+    @Override
+    public int compare(Livro l1, Livro l2){
+        return l1.getNome().compareTo(l2.getNome());
+    }
+}
+
+
+public class NovidadesJava8 {
+
+    public static void main(String[] args) {
+        Autor autor = new Autor();
+        autor.setNome("Rodrigo Turini");
+
+        Livro javaoo = new LivroFisico(autor);
+        javaoo.setNome("Java 0.0.");
+
+        Livro java8 = new LivroFisico(autor);
+        java8.setNome("Java 8 prático");
+
+        Livro ruby = new LivroFisico(autor);
+        ruby.setNome("Livro de Ruby");
+
+        List<Livro> livros = Arrays.asList(javaoo,java8,ruby);
+        Collections.sort(livros, new ComparadorPorNome());
+
+        for (Livro livro : livros)  {
+            System.out.println(livro.getNome());
+        }
+    }
+
+}
