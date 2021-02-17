db7df204f3f21252a1cbf75d789f11959791e36e
Tavares <leandro.tavares@sap.com>
2021-02-17 10:50:06 -0300

    java.util.SeT
    Assim como a List, o Set é apenas uma interface. Para utilizar um Set,
    precisamos instanciar alguma de suas implementações, como a HashSet,
    que é uma das implementações mais usadas. Assim, seguimos programando para
    a interface.
    Ao tentar adicionar um objeto repetido o método add retorna false ignorando
    a adição.
    
    Outra vantagem do Set é que existem implementações mais rápidas do que a list
    para fazer consultas, como o HashSet que utilizamos através do método contains.
    O segredo desse ganho de velocidade está no 'hashcode' e 'equals'.
    
    Tanto Set quanto List implementam a interface 'Collection'

--- src/br/com/casadocodigo/produtos/GerenciadorDeCupons.java
+++ src/br/com/casadocodigo/produtos/GerenciadorDeCupons.java
@@ -1,17 +1,20 @@
 package br.com.casadocodigo.produtos;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GerenciadorDeCupons {
 
-    private List<String> cupons;
+    private Set<String> cupons;
 
     public GerenciadorDeCupons() {
-        this.cupons = Arrays.asList("CUP74", "CUP158", "CUP14", "CUP52", "CUP21",
-                                    "CUP221", "CUP91", "CUP327","CUP410", "CUP275",
-                                    "CUP484", "CUP207", "CUP96", "CUP119", "CUP174",
-                                    "CUP291","CUP1", "CUP115","CUP222", "CUP272");
+        this.cupons = new HashSet<String>();
+        cupons.addAll(Arrays.asList("CUP74", "CUP158", "CUP14",
+                "CUP52", "CUP21","CUP221", "CUP91", "CUP327",
+                "CUP410", "CUP275","CUP484", "CUP207", "CUP96",
+                "CUP119", "CUP174","CUP291","CUP1", "CUP115",
+                "CUP222", "CUP272"));
     }
 
     public boolean validaCupon(String cupom) {
